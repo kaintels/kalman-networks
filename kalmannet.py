166977cde62589c1f94a8792f011715dbc717282
Seungwoo Han <seungwoohan0108@gmail.com>
2023-02-11 15:10:55 +0900

    Update kalmannet

--- kalmannet.py
+++ kalmannet.py
@@ -21,7 +21,6 @@ class KalmanNetV1(nn.Module):
             x_pred = self.A * self.x
             P_pred = self.A * self.p * self.A + self.Q
             K = P_pred * self.H / (self.H * P_pred * self.H + self.R)
-            print(K)
             with torch.no_grad():
                 self.x.copy_(torch.tensor((x_pred + K * (x[u] - self.H * x_pred)).item()))
                 self.p.copy_(torch.tensor((P_pred - K * self.H * P_pred).item()))
@@ -29,29 +28,5 @@ class KalmanNetV1(nn.Module):
 
         return esti_save.reshape(len_sample, -1).requires_grad_(True)
 
-data = torch.ones(10, 1)
-learn_rate = 0.01
-n_epochs = 10
-
-model = KalmanNetV1(x_0=1,p_0=1,A=2,H=1.5,Q=1,R=9)
-mse_loss = nn.MSELoss()
-optimizer = torch.optim.Adam(lr=learn_rate, params=model.parameters())
-
-losses = []
-for p in model.named_parameters():
-	print(p)
-for i in range(0, n_epochs):
-
-    optimizer.zero_grad()
-    y_pred = model(data)
-    loss = mse_loss(y_pred, data)
-    loss.backward()
-    optimizer.step()
-    # print(loss.item())
-
-for p in model.named_parameters():
-	print(p)
-
-X_test = torch.zeros(10, 1, dtype=torch.float32)
-
-print(model(X_test))
+if __name__ == "__main__":
+    model = KalmanNetV1(x_0=1,p_0=1,A=2,H=1.5,Q=1,R=9)
